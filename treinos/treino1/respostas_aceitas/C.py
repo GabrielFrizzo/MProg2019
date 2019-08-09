bc3c4bcaa889e442914d875595aea4c35f108ffa
Lucas Ricardo <39354729+mglucas@users.noreply.github.com>
2019-08-09 15:56:15 -0300

    Tornado C - Commented

--- treinos/treino1/respostas_aceitas/C.py
+++ treinos/treino1/respostas_aceitas/C.py
@@ -1,26 +1,40 @@
+# C - URI 1266 - https://www.urionlinejudge.com.br/judge/en/problems/view/1266
+
 while True:
-    size = int(input())
+    # Input parameters  
+    size = int(input()) 
     if size == 0:
         break
 
     v = [int(i) for i in input().split()]
     count = 0
     i = -1
+    
+    # Find the first "1" in the array
+    # Starts from it
+    # This helps in specific situations with 0 as the first int
     for j in range(len(v)):
         if v[j] == 1:
             i = j
             break
+    # If There is no "1" in the array
+    # Just return the size divided by 2, rounded up
     if i == -1:
         print(int((size+1)/2))
         continue
 
-    abc = i
+    #index keeps the start index
+    index = i
     while True: 
+        # While is checking every pos in array
+        # Looking for a pair of "0"
         if v[i%size] == 0 and v[(i+1)%size] == 0:
             count += 1
             i += 1
+            # If i is equals to the end pos, break
             if i%size == (abc-1)%size:
                 break
+        # Go for the next pos on the array
         i += 1
         if i%size == (abc-1)%size:
             break
