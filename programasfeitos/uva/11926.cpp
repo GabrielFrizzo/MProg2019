b5bcab82557e38514ebf41a819f820a15d9c3b0d
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2019-08-09 17:42:48 -0300

    11926.cpp

---
+++ programasfeitos/uva/11926.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <bitset>
+using namespace std;
+
+bitset<1123456> schedule;
+void run(int n, int m) {
+    int start, end, interval;
+    bool flag = false;
+    schedule.reset();
+    while (n--) {
+        scanf("%d %d", &start, &end);
+        if (flag) {continue;}
+        for (int i=start; i < end; i++) {
+            if (schedule.test(i)) {
+                flag = true;
+            }
+            schedule.set(i);
+        }
+    }
+    while (m--) {
+        scanf("%d %d %d", &start, &end, &interval);
+        if (flag) {continue;}
+        for (int j = 0; j+start <= 1000000; j+= interval) {
+            for (int i = start+j; i<end+j && i <= 1000000; i++) {
+                if (schedule.test(i)) {
+                    flag = true;
+                }
+                schedule.set(i);
+            }
+        }
+    }
+    flag ? printf("CONFLICT\n") : printf("NO CONFLICT\n");
+    return;
+}
+
+int main() {
+    int n, m;
+    while (scanf("%d %d", &n, &m), n || m) {
+        run(n, m);
+    }
+
+
+    return 0;
+}
